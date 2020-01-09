c3c5263ad3be2aab13ad6f2a8246467a2750a3a0
Andrew <andrewc.london2019a@gmail.com>
2020-01-09 10:32:56 +0000

    Inheritance to aggregates part 5 - STUB added, this is a mock of a class that may be being written elsewhere but the tests can be passed using the stub

---
+++ src/main/java/com/qa/aggregates/AAAStub.java
@@ -0,0 +1,13 @@
+package com.qa.aggregates;
+
+public class AAAStub implements IAAA {
+    @Override
+    public int fooBar() {
+        return 0;
+    }
+
+    @Override
+    public void f1() {
+
+    }
+}
